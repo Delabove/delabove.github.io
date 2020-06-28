8a9cbca87d3f453536396cda249111178075e173
DeLayne LaBove <delayne.labove@gmail.com>
2020-06-27 19:00:25 -0500

    Creating a basic carousel structure with Javascript

--- js/carousel.js
+++ js/carousel.js
@@ -2,11 +2,22 @@
 
 console.log("hello.")
 
+// BASIC CAROUSEL CODE
+
+// START
+// IMAGE ARRAY
+// TIME BETWEEN EACH IMAGE
+// FUNCTION TO LOOP THROUGH THE IMAGES
+
+        let slideStart = 0;
+        let imageArray = [];
+        let time = 5000;
+
+        imageArray[0] = "Project-GIFS/Adlister.gif";
+        imageArray[1] = "Project-GIFS/coffee-project.gif";
+        imageArray[2] = "Project-GIFS/weatherMap.gif";
 
 
+function changingImages() {
 
-function myFunction() {
-    document.getElementById("coffee").style.visibility = "hidden";
-
-    console.log("clicked")
 }
